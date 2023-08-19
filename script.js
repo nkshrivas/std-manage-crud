9fc7e10ce69532b5e80ae3ae8fa10bc882e18d78
nkshrivas <nk733095@gmail.com>
2023-08-19 17:18:04 +0530

    crud performed

---
+++ script.js
@@ -0,0 +1,73 @@
+let table = document.getElementById("table");
+let btn = document.getElementById("add-btn");
+
+btn.addEventListener("click", function () {
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let email = document.getElementById("email").value;
+    let gpa = document.getElementById("gpa").value;
+    let degree = document.getElementById("degree").value;
+
+    if (name && age && email && gpa && degree) {
+        let row = table.insertRow();
+        
+        let cell = row.insertCell();
+        cell.innerHTML = table.rows.length - 1; // Display row number
+        
+        cell = row.insertCell();
+        cell.innerHTML = name;
+        
+        cell = row.insertCell();
+        cell.innerHTML = age;
+        
+        cell = row.insertCell();
+        cell.innerHTML = email;
+        
+        cell = row.insertCell();
+        cell.innerHTML = gpa;
+        
+        cell = row.insertCell();
+        cell.innerHTML = degree;
+        
+        cell = row.insertCell();
+        cell.innerHTML = `<button class="btn btn-danger" onclick="deleteRow(this)">Delete</button>`;
+        cell.innerHTML += `<button class="btn btn-warning" onclick="editRow(this)">Edit</button>`;
+        
+        clearInputFields();
+    } else {
+        alert("Please fill in all the fields.");
+    }
+});
+
+function deleteRow(button) {
+    let row = button.parentNode.parentNode;
+    table.deleteRow(row.rowIndex);
+}
+
+function editRow(button) {
+    let row = button.parentNode.parentNode;
+    let cells = row.cells;
+    document.getElementById("add-btn").innerHTML = "Edit Student";
+    document.getElementById("add-btn").style.backgroundColor = "#ffc107";
+
+    document.getElementById("name").value = cells[1].innerHTML;
+    document.getElementById("age").value = cells[2].innerHTML;
+    document.getElementById("email").value = cells[3].innerHTML;
+    document.getElementById("gpa").value = cells[4].innerHTML;
+    document.getElementById("degree").value = cells[5].innerHTML;
+    
+    table.deleteRow(row.rowIndex);
+
+    document.getElementById("add-btn").addEventListener("click", function () {
+        document.getElementById("add-btn").innerHTML = "Add Student";
+        document.getElementById("add-btn").style.backgroundColor = "#fff";
+});
+}
+
+function clearInputFields() {
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("gpa").value = "";
+    document.getElementById("degree").value = "";
+}
